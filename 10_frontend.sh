a5f1eb1c2bb3734cf2acde347b14d909014f5e60
VinodDevOpsTech <vinodkumar160107@gmail.com>
2026-05-30 18:44:23 +0530

    hello

--- 10_frontend.sh
+++ 10_frontend.sh
@@ -20,7 +20,7 @@ VALIDATE $? "Downloaded and extracted frontend code"
 rm -rf /etc/nginx/nginx.conf
 VALIDATE $? "Removed Default conf"
 
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+cp $SCRIPT_DIR/nginx.config /etc/nginx/nginx.conf
 VALIDATE $? "Copied roboshop nginx conf"
 
 systemctl restart nginx
